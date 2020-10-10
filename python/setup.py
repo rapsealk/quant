dee00cc993271ceaddf1430ba9551dfe4d57cf0a
rapsealk <piono623@naver.com>
2020-10-10 15:29:52 +0900

    Add: setup.py

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='quant',
+    version='0.1.0',
+    packages=['quant'],
+    url='https://github.com/rapsealk/quant',
+    license='MIT',  # FIXME
+    author='rapsealk',
+    author_email='piono623@naver.com',
+    description='A Python implementation of the quant package from R.'
+)
